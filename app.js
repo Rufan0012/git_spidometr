3889d6a45e97eacf439bfdbed09f74f0ce9c56b8
Rufan0012 <108839185+Rufan0012@users.noreply.github.com>
2022-08-27 13:30:26 -0700

    Update app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const Tormoz=document.getElementById(`Tormoz`)
+const QazVer=document.getElementById(`QazVer`)
+
+eqreb.style.transform = `rotate(${suret}deq)`
+
+Tormoz.onclick=function(){
+    if(suret<=0){
+        suret=0
+        console.log(`bele sey olmur`)
+        eqreb.style.transform=`rotate(${suret}deg)`
+        return;
+    }
+    suret=suret-10
+    eqreb.style.transform=`rotate(${suret}deg)`
+}
+QazVer.onclick=function(){
+    if(suret>=180){
+        suret=180
+        eqreb.style.transform=`rotate(${suret}deg)`
+        console.log(`motor dagilacaq!!!`)
+        return;
+    }
+    suret=suret+10
+    eqreb.style.transform=`rotate(${suret}deg)`
+} 
